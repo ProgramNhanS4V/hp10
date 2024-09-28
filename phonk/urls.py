a12cace5d33edd69e95c195754c6eec263b6dfe8
TienCoi207201 <namtien9182@gmail.com>
2024-09-28 16:45:37 +0700

    update templates

--- phonk/urls.py
+++ phonk/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('home', views.home, name = "home"),
     path('signin', views.signin, name = "signin"),
     path('signup', views.signup, name = "signup"),
+    path('detail/<int:id>', views.detail, name = "detail"),
 ]
--- phonk/views.py
+++ phonk/views.py
@@ -5,8 +5,13 @@ from .models import Product
 
 def home(req):
     product = Product.objects.all()
+    print(product)
     return render(req, 'home.html', context={ 'product': product})
 def signin(req):
     return render(req, 'signin.html')
 def signup(req):
     return render(req, 'signup.html')
+
+def detail(req, id):
+    product = Product.objects.get(id = id)
+    return render(req, 'product_detail.html', context={ 'product': product})
